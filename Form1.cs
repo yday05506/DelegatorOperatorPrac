2e1e7327e4f1ba4f61595ebb598463422ba12952
yday05506 <yangdayeon2@gmail.com>
2023-08-31 09:40:13 +0900

    0831

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,7 @@ namespace DelegatorOperatorPrac
 {
     public partial class Form1 : Form
     {
+        public delegate void SendString(string message);
         public Form1()
         {
             InitializeComponent();
@@ -20,10 +21,37 @@ namespace DelegatorOperatorPrac
         private void Form1_Load(object sender, EventArgs e)
         {
             Button button = new Button();
+
             button.Text = "나만의 델리게이터 버튼";
             button.Click += button_click1;
-            button.Click += button_click2;
+            button.Click += (s, e2) => { MessageBox.Show("잘 가"); };
+
             Controls.Add(button);
+
+            SendString sayHello, sayGoodbye, multiDelegate;
+            sayHello = Hello;
+            sayGoodbye = Goodbye;
+            multiDelegate = sayHello + sayGoodbye;
+
+            multiDelegate("아이유");
+
+            multiDelegate -= sayGoodbye;
+            multiDelegate("아이유");
+        }
+
+        private void Goodbye(string message)
+        {
+            Console.WriteLine("안녕히 가세요, " + message + "님!");
+        }
+
+        private void Hello(string message)
+        {
+            Console.WriteLine("안녕하세요, " + message + "님!");
+        }
+
+        private void button_click1(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕");
         }
     }
 }
