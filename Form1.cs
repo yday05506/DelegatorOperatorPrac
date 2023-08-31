c181775187e4072d1033ebd2c79bddafd5c05981
yday05506 <yangdayeon2@gmail.com>
2023-08-31 09:12:47 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegatorOperatorPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button button = new Button();
+            button.Text = "나만의 델리게이터 버튼";
+            button.Click += button_click1;
+            button.Click += button_click2;
+            Controls.Add(button);
+        }
+    }
+}
